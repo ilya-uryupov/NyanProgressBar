e7397d8d301e621b3c0cd1bb4adf573a93b5e722
Ilya Uryupov <ilya.uryupov@gmail.com>
2019-07-13 15:21:59 +0300

    Revert to old API.

--- src/NyanApplicationComponent.java
+++ src/NyanApplicationComponent.java
@@ -1,15 +1,39 @@
-import com.intellij.ide.ui.LafManager;
+import com.intellij.ide.ui.laf.LafManagerImpl;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.ApplicationComponent;
+
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
-public class NyanApplicationComponent {
-    public NyanApplicationComponent() {
-        LafManager.getInstance().addLafManagerListener(__ -> updateProgressBarUi());
+public class NyanApplicationComponent implements ApplicationComponent {
+    public NyanApplicationComponent(LafManagerImpl lafManager) {
+        lafManager.addLafManagerListener(lafManager1 -> updateProgressBarUi());
+    }
+
+    @Override
+    public void initComponent() {
         updateProgressBarUi();
+    }
+
+    @Override
+    public void disposeComponent() {
+
+    }
+
+    @NotNull
+    @Override
+
+    public String getComponentName() {
+        return "NyanLafUpdater";
     }
 
     private void updateProgressBarUi() {
         UIManager.put("ProgressBarUI", NyanProgressBarUi.class.getName());
         UIManager.getDefaults().put(NyanProgressBarUi.class.getName(), NyanProgressBarUi.class);
+    }
+
+    static NyanApplicationComponent getInstance() {
+        return ApplicationManager.getApplication().getComponent(NyanApplicationComponent.class);
     }
 }
